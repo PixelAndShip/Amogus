870999f060cd7aef54c097cda2cbbb3cbae1661a
PixelAndShip <72350325+PixelAndShip@users.noreply.github.com>
2022-05-19 08:41:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('btn');
+
+
+
+
+btn.addEventListener('click', function onClick(event) {
+  // 👇️ change background color
+  
+
+function onClick(){
+console.log('1');
+  if(document.body.style.backgroundImage = "/maxresdefault.jpg") {
+      document.body.style.backgroundImage ="/E_cxow9XoAQCZHu.jpg";
+  } else {
+      document.body.style.backgroundImage = "/maxresdefault.jpg";
+  }
+}
+  // 👇️ optionally change text color
+  // event.target.style.color = 'white';
+});
